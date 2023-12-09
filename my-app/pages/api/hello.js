d71b4b922c6353a0c9b9ad518435550e94d94d3a
yuqy <shooting.star4242@gmail.com>
2023-12-09 16:10:38 +0900

    Update

--- my-app/pages/api/hello.js
+++ my-app/pages/api/hello.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: 'edge',
 }
 
-export default async function (req) {
-  const kvvalue = await process.env.KV.get('test')
-  return new Response(kvvalue)
+export default async function getServerSideProps() {
+  const random = Math.floor(Math.random() * 100);
+  return new Response(random);
 }
